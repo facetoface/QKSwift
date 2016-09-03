8f5d15165347d3708435380a0734b9c9de979501
言志 <1392984568@qq.com>
2016-09-03 16:23:28 +0800

    Initial Commit

---
+++ QKSwiftTests/QKSwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  QKSwiftTests.swift
+//  QKSwiftTests
+//
+//  Created by 一米阳光 on 16/9/3.
+//  Copyright © 2016年 一米阳光. All rights reserved.
+//
+
+import XCTest
+@testable import QKSwift
+
+class QKSwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
