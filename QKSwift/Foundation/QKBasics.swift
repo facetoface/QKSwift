c841ead6616d35e3fbe678db661deaae6a460474
EasyIOS <1392984568@QQ.com>
2016-10-20 09:13:48 +0800

    升级到swift3

--- QKSwift/Foundation/QKBasics.swift
+++ QKSwift/Foundation/QKBasics.swift
@@ -190,7 +190,7 @@ class QKBasics: NSObject {
         print("\n----- while lopps -----")
         let finalSquare = 25
        
-        var board = [Int](count: finalSquare + 1,repeatedValue: 0)
+        var board = [Int](repeating: 0,count: finalSquare + 1)
         board[03] = +08; board[06] = +11; board[09] = +09; board[10] = +02
         board[14] = -10; board[19] = -11; board[22] = -02; board[24] = -08
         var square = 0
