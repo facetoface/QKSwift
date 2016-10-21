f7c2f5700933dd967c1d200aa5a60c1410097cae
EasyIOS <1392984568@QQ.com>
2016-10-21 18:25:13 +0800

    添加注释

--- QKSwift/Foundation/QKStudyClosure.swift
+++ QKSwift/Foundation/QKStudyClosure.swift
@@ -6,8 +6,16 @@
 //  Copyright © 2016年 一米阳光. All rights reserved.
 //
 // Closure Expression syntax
+// The parameters in closure expression syntax can be in-out parameters, but they can't have a default value. Variadic parameters can be used if you name the variadic parameter. Tuples can also be used as parameter types and return types. 
+// The example below shows a closure expression version of the backward(_:_:) function from earlier:
 import UIKit
 
 class QKStudyClosure: NSObject {
 
+    func test(){
+      
+    }
+    
+    
+    
 }
