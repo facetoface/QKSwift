98bf57abfb548759a7b3962138875be719aa9ec5
EasyIOS <1392984568@QQ.com>
2016-10-21 18:12:03 +0800

    闭包

---
+++ QKSwift/Foundation/QKStudyClosure.swift
@@ -0,0 +1,13 @@
+//
+//  QKStudyClosure.swift
+//  QKSwift
+//
+//  Created by ChiCo on 16/10/21.
+//  Copyright © 2016年 一米阳光. All rights reserved.
+//
+// Closure Expression syntax
+import UIKit
+
+class QKStudyClosure: NSObject {
+
+}
