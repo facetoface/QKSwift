c87b32e5d36ee6878129063bc04101b21185cc62
EasyIOS <1392984568@QQ.com>
2016-10-25 10:27:16 +0800

    函数作为返回值

--- QKSwift/Foundation/FoundationPlayGround.playground/Contents.swift
+++ QKSwift/Foundation/FoundationPlayGround.playground/Contents.swift
@@ -3,12 +3,17 @@
 import UIKit
 
 
-    //返回函数 n1 是返回的函数的参数 后面的Int 是这个函数的返回值
+    //->(_ n1: Int) 代表一个函数，我们把它叫做 A吧
+    //在 A 后面->Int是他的返回值
+    //myFunction(forIncrement amount: Int) 这是一个叫myFunction的函数
+    //拥有一个Int参数amount 其中forIncrement可以提高阅读性，我把它称作参数标签
+    //那么A和她后面的->Int 是这个函数的返回值
     func myFunction(forIncrement amount: Int) ->(_ n1: Int) ->Int{
         
+        print(amount)
         func ahah(number1 number: Int) -> Int{
             
-            return number + 2
+            return number + amount
             
         }
         return ahah;
@@ -17,5 +22,4 @@ import UIKit
     let myTest = myFunction(forIncrement: 10)
     print(myTest(3))
     print(myTest(4))
-    
     print(myFunction(forIncrement: 1))
