b7566e41f0ed283163fe947cc8862a459735cd09
EasyIOS <1392984568@QQ.com>
2016-11-01 18:37:10 +0800

    开始看class

---
+++ QKSwift/Foundation/QKStudyClass.swift
@@ -0,0 +1,32 @@
+//
+//  QKStudyClass.swift
+//  QKSwift
+//
+//  Created by ChiCo on 16/11/1.
+//  Copyright © 2016年 一米阳光. All rights reserved.
+//
+// Clas and Structures
+
+import UIKit
+
+//Class and structures are general-purpose, flexible constructs that become the building blocks of your program's code. You define properties and methods to add functionality to your classes and structures by using exactly the same syntax as for constants, variables, and functions.
+//Unlike other programming languages, Swift does not require you to create separate interface and implementation files for custome classes and structures. In Swift, you define a class or a structure in a single file, and the exterrnal interface to that class or structure is automatically made available for other code to use.
+//NOTE An instance of a class is traditionally known as an object. However, Swift classes and structures are much closer in functionality than in other languages,and much of this chapter describes functionality that can apply to instances of either a class or a structure type. Because of this, the more genearl term instance is used.
+
+//Comparing Classes and Structures
+//Class and structures in Swift have many things in common.Both can:
+//Define properties to store values
+//Define methods to provide functionality
+//Define subscripts to provide access to their values using subscript syntax
+//Define initializers to set up their initial state
+//Be extended to expand their functionality beyond a default implementation
+//Conform to protocols to provide standard functionality of a certain kind
+
+//Class have additional capabilities that stuctures do not:
+//Inheritance enables one class to inherit the characteristics of another.
+//Type casting enables you to check and interpret the type of a class instance at runtime.
+
+class QKStudyClass: NSObject {
+
+}
+
