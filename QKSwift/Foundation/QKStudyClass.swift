5aad72632d6c80d49821a05b87d910b12f2ab03c
EasyIOS <1392984568@QQ.com>
2016-11-07 18:45:47 +0800

    类中和stuct 中使用点语法

--- QKSwift/Foundation/QKStudyClass.swift
+++ QKSwift/Foundation/QKStudyClass.swift
@@ -42,9 +42,24 @@ class VideoModel {
     var name: String?
 }
 //The example above defines a new structure called Resolution, to describe a pixel-based display resolution. This structure has two stored properties called width and height. Stored properties are constants or variables that are bundled up and stored as part of the class or structure. Thes two properties are inferred to be of type Int by setting them to an initial integer value of 0.
-//The example above also defines a new class called VideoMode, to describe a specific video mode for video display. This class has four variable stored properties. The first, resolution, is initialized with a new Resolution stucture instance, which infers a property type of Resolution, For the other three properties, new VideoMode instances will be initialized with an interlaced setting of false(meaning "noninterlaced video"), a playback frame rate of 0.0,
+//The example above also defines a new class called VideoMode, to describe a specific video mode for video display. This class has four variable stored properties. The first, resolution, is initialized with a new Resolution stucture instance, which infers a property type of Resolution, For the other three properties, new VideoMode instances will be initialized with an interlaced setting of false(meaning "noninterlaced video"), a playback frame rate of 0.0, and an optional String value called name. The name property is automatically given a default value of nil, or "no name value", because it is of an optional type.
+//Class and Structure Instances
+//The Resolution structure definition and the VideoMode class definition only describe what a Resolution or VideoMode will look like. They themselves do not describe a specific resolution or video mode. To do that, you need to create an instance of the structure or class.
+//The synatax for creating instances is very similar for both stuctures and classes
 
 class QKStudyClass: NSObject {
 
+    //Structures and classes both use initializer syntax for new instances. The simplest from of initializer syntax uses the type name of the class or structure followed by empty parentheses, such as Resolution() or VideoMode(). This creates a new instance of the class or structure, with any properties initialized to their default values.
+    let someResolution = Resolution()
+    let someVideoMode = VideoModel();
+    
+    //Accessing Properties
+    //YOu can access the properties of an instance using dot syntax. in dot syntax., you write the property name immediately after the instance name, separated by a period (.), without any spaces:
+    
+    func test(){
+    
+        print("The width of someResolution is \(someResolution.width)")
+    
+    }
 }
 
